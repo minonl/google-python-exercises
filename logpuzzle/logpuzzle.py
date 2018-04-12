7c2048c198b41f16145dab2c42ac519c090bf1f6
ZHAO Min <min@veda.studio>
2018-04-13 00:29:31 +0800

    finished log-puzzle

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -17,8 +17,12 @@ Given an apache logfile, find the puzzle urls and download the images.
 Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
-
-
+def sort_key(filename):
+  match = re.search(r'(-\w\w\w\w)\.jpg$', filename)
+  if match:
+        return match.group(1)
+  else: return filename
+    
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
@@ -31,12 +35,12 @@ def read_urls(filename):
   urls = []
   f = open(filename,'rU')
   for line in f:
-        match = re.search(r'GET\s([\/]\S*[puzzle]+\S*\.jpg)', line)
+        match = re.search(r'GET\s([\/]\S*[puzzle]+\S*(-\w\w\w\w)\.jpg)', line)
         if match:
-          url = 'http://code.gogle.com'+match.group(1)
+          url = 'http://code.google.com'+match.group(1)
           if url not in urls:
-            urls.append('http://code.gogle.com'+match.group(1))
-  return sorted(urls)
+            urls.append(url)
+  return sorted(urls, key=sort_key)
 
 def download_images(img_urls, dest_dir):
   """Given the urls already in the correct order, downloads
@@ -47,7 +51,18 @@ def download_images(img_urls, dest_dir):
   Creates the directory if necessary.
   """
   # +++your code here+++
-  
+  if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+  f = open(os.path.join(dest_dir,'index.html'),'w')
+  f.write('<verbatim>\n<html>\n<body>\n')
+  for no in range(len(img_urls)-1):
+    filename = str(no+1)+'.jpg'
+    path = os.path.join(dest_dir, filename)
+    urllib.urlretrieve(img_urls[no],path)
+    print 'Retrieving...', img_urls[no]
+    f.write('<img src=%s>' % filename)
+  f.write('\n</body>\n</html>')
+  f.close()
 
 def main():
   args = sys.argv[1:]
