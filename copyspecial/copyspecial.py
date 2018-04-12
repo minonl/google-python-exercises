010f358881e387e9693ed394a8a9a34178d0d5c1
ZHAO Min <min@veda.studio>
2018-04-12 22:40:50 +0800

    copy-special part B

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -17,52 +17,73 @@ import commands
 
 # +++your code here+++
 # Write functions and modify main() to call them
+
+
 def get_special_paths(dir):
     special_paths = []
     try:
-      filenames = os.listdir(dir)
-      for filename in filenames:
-        match = re.search(r'\_\_[\w]+\_\_', filename)
-        if match:
-          special_paths.append(os.path.abspath(os.path.join(dir, filename)))
+        filenames = os.listdir(dir)
+        for filename in filenames:
+            match = re.search(r'\_\_[\w]+\_\_', filename)
+            if match:
+                special_paths.append(os.path.abspath(
+                    os.path.join(dir, filename)))
     except IOError:
-      sys.stderr.write('error reading:'+dir)
+        sys.stderr.write('error reading: '+dir)
     else:
-      for path in special_paths:
-        print path
-      return special_paths
+        #for path in special_paths:
+        #    print path
+        return special_paths
+
+
+def copy_to(paths, dir):
+    try:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+    except IOError:
+        sys.stderr.write('directory failure: '+dir)
+    else:
+        try:
+            for path in paths:
+                shutil.copy(path, os.path.join(dir, os.path.basename(path)))
+        except IOError:
+            sys.stderr.write('copy failure: [from]'+paths+'|[to]'+dir)
+
 
 def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
+    # This basic command line argument parsing code is provided.
+    # Add code to call your functions below.
 
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-  if not args:
-    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
-    sys.exit(1)
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
+    if not args:
+        print "usage: [--todir dir][--tozip zipfile] dir [dir ...]"
+        sys.exit(1)
 
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    # todir and tozip are either set from command line
+    # or left as the empty string.
+    # The args array is left just containing the dirs.
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  tozip = ''
-  if args[0] == '--tozip':
-    tozip = args[1]
-    del args[0:2]
+    tozip = ''
+    if args[0] == '--tozip':
+        tozip = args[1]
+        del args[0:2]
 
-  if len(args) == 0:
-    print "error: must specify one or more dirs"
-    sys.exit(1)
+    if len(args) == 0:
+        print "error: must specify one or more dirs"
+        sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
-  get_special_paths(args[0])
-  
+    # +++your code here+++
+    # Call your functions
+    paths = get_special_paths(args[0])
+    if len(todir)>0 :
+      copy_to(paths,todir)
+
+
 if __name__ == "__main__":
-  main()
+    main()
